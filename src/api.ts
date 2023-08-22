32c8d4935107661183141cca47918a9ccb9a5977
Junli Li <jungle216@126.com>
2023-08-22 22:20:33 +0800

    move publickey options to BE

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,13 @@
-export async function fetchChallenge(bodyData: { username: string; type: string }) {
-  const response = await fetch("http://localhost:3030/generate-challenge", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(bodyData),
-  });
+import { base64URLDecode } from "./helper";
+
+export async function fetchOptions({ username, type }: { username: string; type: string }) {
+  const response = await fetch(`http://localhost:3030/generate-options?username=${username}&type=${type}`);
 
   return response;
 }
+
+export const parsedRegisterOptions = (options) => ({
+  ...options,
+  challenge: base64URLDecode(options.challenge),
+  user: { ...options.user, id: base64URLDecode(options.user.id) },
+});
